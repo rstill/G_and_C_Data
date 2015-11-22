2f348a626d19ac87d15ee963567f2ddba4b49a80
Ray Still <rstill@Rays-iMac.local>
2015-11-22 10:53:49 -0800

    Added time information to the code book.

--- src/CodeBook.md
+++ src/CodeBook.md
@@ -5,8 +5,8 @@
 _The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz._
 _Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag)._
 #### The columns for each observation are as follows.
-* __activity__ -- The activity being conducted when the observation was made.
-* __subject__ -- The subject conducting the activity when the observation was made.
+* __activity.__ The activity being conducted when the observation was made.
+* __subject.__ The subject conducting the activity when the observation was made.
 * __tBodyAcc-mean()-X.__ Mean of tBodyAcc-mean()-X for all observations of the activity conducted by the indicated subject.
 * __tBodyAcc-mean()-Y.__ Mean of tBodyAcc-mean()-Y for all observations of the activity conducted by the indicated subject.
 * __tBodyAcc-mean()-Z.__ Mean of tBodyAcc-mean()-Z for all observations of the activity conducted by the indicated subject.
--- src/run_analysis.R
+++ src/run_analysis.R
@@ -197,8 +197,8 @@ generate_code_book <- function(data, labels) {
 				   "_The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz._",
 				   "_Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag)._",
 				   "#### The columns for each observation are as follows.",
-			   	   "* __activity__ -- The activity being conducted when the observation was made.", 
-			       "* __subject__ -- The subject conducting the activity when the observation was made.")
+			   	   "* __activity.__ The activity being conducted when the observation was made.", 
+			       "* __subject.__ The subject conducting the activity when the observation was made.")
 	features <- names(data)
 	features <- features[!features %in% c("activity", "subject")]
 	codes <- sapply(features, function(feature) { sprintf("* __%s.__ Mean of %s for all observations of the activity conducted by the indicated subject.", feature, feature) })
